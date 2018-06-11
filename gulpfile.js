fe3ded2e94888dec4c224ead51475b4b68b0c89b
Enzo Domingues <enzo.dominguees@gmail.com>
2018-06-11 02:31:31 -0300

    gulp configured

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var connect = require('gulp-connect');
+
+
+gulp.task('default',["sass","html","connect","watch"], function() {
+})
+ 
+gulp.task('sass', function () {
+  return gulp.src('./src/assets/styles/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/assets/styles/css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('./src/css/**/*.scss', ['sass']),
+  gulp.watch(['./src/html/**/*.html'], ['html']);
+});
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'src',
+    livereload: true
+  });
+});
+ 
+gulp.task('html', function () {
+  gulp.src('./src/html/**/*.html')
+    .pipe(gulp.dest('./dist/html'))
+    .pipe(connect.reload());
+});
+
+ 
---
+++ paths.js
@@ -0,0 +1,19 @@
+module.exports = {
+    src: './src',
+    dist: './public/wp-content/themes/royalpixel',
+    images: {
+        src: './src/img',
+        dist: './public/wp-content/themes/royalpixel/assets/img'
+    },
+    styles: {
+        src: './src/styl',
+        dist: './public/wp-content/themes/royalpixel'
+    },
+    scripts: {
+        src: './src/js',
+        dist: './public/wp-content/themes/royalpixel/assets/js'
+    },
+    vendors: {
+        src: './src/vendors',
+    }
+}
